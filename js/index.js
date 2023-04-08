fa55becc782108686ac6e5685ebda91dee53de64
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2023-04-08 20:43:53 +0600

    feat: color is updated

--- js/index.js
+++ js/index.js
@@ -6,9 +6,9 @@ const form = document.getElementById("form");
 const part1 = document.getElementById("part1");
 const part2 = document.getElementById("part2");
 const part3 = document.getElementById("part3");
-const step1 = document.getElementById('step1');
-const step2 = document.getElementById('step2');
-const step3 = document.getElementById('step3');
+const step1 = document.getElementById("step1");
+const step2 = document.getElementById("step2");
+const step3 = document.getElementById("step3");
 
 
 form.addEventListener("submit", (e) => {
@@ -17,38 +17,37 @@ form.addEventListener("submit", (e) => {
 });
 
 nextBtn1.addEventListener("click", () => {
-   part1.classList.remove('block');
-   part1.classList.add('none');
-   part2.classList.remove('none');   
-   part2.classList.add('block');
-   step1.classList.add('medium'); 
-   step2.classList.add('normal'); 
+   part1.classList.remove("block");
+   part1.classList.add("none");
+   part2.classList.remove("none");
+   part2.classList.add("block");
+   step1.classList.add("medium");
+   step2.classList.add("normal");
 });
 
 prevBtn1.addEventListener("click", () => {
-   part1.classList.add('block');
-   part1.classList.remove('none');
-   part2.classList.add('none');   
-   part2.classList.remove('block');
-   step1.classList.remove('medium');
-   step2.classList.remove('normal'); 
+   part1.classList.add("block");
+   part1.classList.remove("none");
+   part2.classList.add("none");
+   part2.classList.remove("block");
+   step1.classList.remove("medium");
+   step2.classList.remove("normal");
 });
 
 nextBtn2.addEventListener("click", () => {
-   part3.classList.add('block');
-   part3.classList.remove('none');
-   part2.classList.add('none');   
-   part2.classList.remove('block');
-   step1.classList.add('medium'); 
-   step2.classList.add('medium'); 
-   step2.classList.remove('normal'); 
-   step3.classList.add('normal'); 
+   part3.classList.add("block");
+   part3.classList.remove("none");
+   part2.classList.add("none");
+   part2.classList.remove("block");
+   step1.classList.add("medium");
+   step2.classList.add("medium");
+   step2.classList.remove("normal");
+   step3.classList.add("normal");
 });
 nextBtn3.addEventListener("click", () => {
-   step1.classList.remove('medium');  
-   step1.classList.add('full');
-   step2.classList.remove('medium'); 
-   step2.classList.add('full'); 
-   step3.classList.add('full');
+   step1.classList.remove("medium");
+   step1.classList.add("full");
+   step2.classList.remove("medium");
+   step2.classList.add("full");
+   step3.classList.add("full");
 });
-
